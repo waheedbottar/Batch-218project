42509990a342dccabc4c5ec5fe331c1ad1296685
wahee <wahee@DESKTOP-R31ERA0>
2017-05-30 19:37:29 -0400

    moving all files

---
+++ YahooProject/src/test/java/Com/Login/Test1.java
@@ -0,0 +1,14 @@
+package Com.Login;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		//System.out.println("this test1");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+
+	}
+
+}
